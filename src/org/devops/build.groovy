cfc5b1eb31dbabd13e6795dd5263c623fe99136a
pop9009 <34903457+pop9009@users.noreply.github.com>
2020-08-25 11:08:20 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -1,5 +1,11 @@
 package org.devops
 
 def build(buildType,buildShell){
-  buildTools=["mvn":"M2"]
+  buildTools=["mvn":"M2", "gradle":"GRADLE", "npm":"NPM"]
+  buildHome = tool buildTools[buildType]
+  println(buildHome地址 "${buildHome}")
+  if ("${buildType}" = "npm"){
+    sh "export NODE_HOME=${buildHome} && export PATH=\$NODE_HOME/bin:\$PATH && ${buildHome}/bin/npm ${BuildShell}"
+  }
+  sh "${buildHome}/bin/${buildType} ${buildShell}"
 }
