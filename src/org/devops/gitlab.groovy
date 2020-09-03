1e6a8fbeb04dd5a82fb8ceae5e81eb707d8a682d
pop9009 <34903457+pop9009@users.noreply.github.com>
2020-09-03 16:53:30 +0800

    Update gitlab.groovy

--- src/org/devops/gitlab.groovy
+++ src/org/devops/gitlab.groovy
@@ -3,7 +3,7 @@ package org.devops
 //封装HTTP请求
 def HttpReq(reqType,reqUrl,reqBody){
     def gitServer = "http://172.100.25.65:8880/api/v4"
-    withCredentials([string(credentialsId: 'gitlab-token', variable: 'gitlabToken')]) {
+    withCredentials([string(credentialsId: 'gitlab', variable: 'gitlabToken')]) {
       result = httpRequest customHeaders: [[maskValue: true, name: 'PRIVATE-TOKEN', value: "${gitlabToken}"]], 
                 httpMode: reqType, 
                 contentType: "APPLICATION_JSON",
