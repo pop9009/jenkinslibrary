043746de67c71092e531a0caf5d0f7d05a0b6b3f
pop9009 <34903457+pop9009@users.noreply.github.com>
2020-09-14 15:56:49 +0800

    Update sonarqube.groovy

--- src/org/devops/sonarqube.groovy
+++ src/org/devops/sonarqube.groovy
@@ -1,13 +1,13 @@
 package org.devops
 
 def SonarScan(projectName,projectDesc,projectPath){
-  withSonarQubeEnv("sonarqube-test"){
-    def scannerHome= "/usr/local/sonar-scanner-4.4.0.2170-linux/"
-//  def sonarServer= "http://172.100.25.65:9000"
-    def sonarDate= sh returnStdout: true, script: 'date +%Y%m%d%H%S'
-    sonarDate = sonarDate -"\n"
-      sh"""
-      ${scannerHome}/bin/sonar-scanner-Dsonar.projectKey=${projectName} \
+  def scannerHome= "/usr/local/sonar-scanner-4.4.0.2170-linux/"
+  def sonarServer= "http://172.100.25.65:9000"
+  def sonarDate= sh returnStdout: true, script: 'date +%Y%m%d%H%S'
+  sonarDate = sonarDate -"\n"
+  sh"""
+      ${scannerHome}/bin/sonar-scanner -Dsonar.host.url=${sonarServer} \
+      -Dsonar.projectKey=${projectName} \
       -Dsonar.projectName=${projectName} \
       -Dsonar.projectVersion=${sonarDate} \
       -Dsonar.login=admin \
@@ -21,5 +21,4 @@ def SonarScan(projectName,projectDesc,projectPath){
       -Dsonar.java.test.binaries=target/test-classes \
       -Dsonar.java.surefire.report=target/surefire-reports
   """
-  }
 }
