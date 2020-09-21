fad8f9636be6dac04f8d446d84f6eb4e79f76b57
pop9009 <34903457+pop9009@users.noreply.github.com>
2020-09-21 14:26:08 +0800

    Create sonarapi.groovy

---
+++ src/org/devops/sonarapi.groovy
@@ -0,0 +1,25 @@
+package org.devops
+
+//封装HTTP请求
+def HttpReq(reqType,reqUrl,reqBody){
+    def sonarServer = "http://172.100.25.65:9000/api"
+    result = httpRequest authentication: 'sonar-admin-user', 
+              httpMode: reqType, 
+              contentType: "APPLICATION_JSON",
+              consoleLogResponseBody: true,
+              ignoreSslErrors: true, 
+              requestBody: reqBody,
+              url: "${sonarServer}/${reqUrl}"
+                //quiet: true
+    return result
+}
+
+//获取Sonar质量阈状态
+def getProjectStatus(projectName){
+    apiUrl = "project_branches/list?project=${projectName}"
+    response = HttpReq("GET",apiUrl,"")
+    response = readJSON text: """${response.content}"""
+    println(response)
+    result = response[branches][0][status][qualityGateStatus]
+    return result
+}
